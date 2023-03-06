905bf040b0d984e40c54d69fccf1f33617d23cef
yeonfish6040 <yeonfish6040@gmail.com>
2023-03-06 19:15:37 +0900

    [안미정] - project 시작 - test 'hello'

---
+++ src/main/java/com/duboomom/iLikeSchool/ILikeSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.duboomom.iLikeSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILikeSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILikeSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duboomom/iLikeSchool/ILikeSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duboomom.iLikeSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILikeSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
